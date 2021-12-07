f8c5cc1b5b34fb4367f5a2ef6d81e1d763934f60
ath0s <861482+ath0s@users.noreply.github.com>
2021-12-07 20:22:53 +0100

    Precalculate fuel consumption per move into an array

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -6,19 +6,21 @@ fun crabs(filename: String, verbose: Boolean, fuelPerStep: (Int) -> Int): Int =
     filename.asPath()
         .readLines()
         .let { lines ->
-            val positions = lines[0].split(",").map { it.toInt() }
+            val positions = lines[0].split(",").map { it.toInt() }.toIntArray()
             val min = positions.minOrNull()!!
             val max = positions.maxOrNull()!!
-            val cache = mutableMapOf<Int, Int>()
+            val cache = IntArray(max + 1) {
+                (1..it).sumOf(fuelPerStep)
+            }
             val (position, fuel) = (min..max).fold(-1 to Integer.MAX_VALUE) { (bestPosition, bestFuel), currentPosition ->
-                val currentFuel = calculateFuelIfLessThan(cache, positions, currentPosition, bestFuel, fuelPerStep)
+                val currentFuel = calculateFuelIfLessThan(cache, positions, currentPosition, bestFuel)
                 if (currentFuel != null) {
                     currentPosition to currentFuel
                 } else {
                     bestPosition to bestFuel
                 }
             }
-            if(verbose) {
+            if (verbose) {
                 println("Best position:\t$position, fuel: $fuel")
             }
 
@@ -26,16 +28,15 @@ fun crabs(filename: String, verbose: Boolean, fuelPerStep: (Int) -> Int): Int =
         }
 
 private fun calculateFuelIfLessThan(
-    cache: MutableMap<Int, Int>,
-    positions: Iterable<Int>,
+    cache: IntArray,
+    positions: IntArray,
     currentPosition: Int,
-    bestFuel: Int,
-    fuelPerStep: (Int) -> Int
+    bestFuel: Int
 ): Int? {
     var sum = 0
     for (position in positions) {
         val moves = abs(currentPosition - position)
-        val fuel = cache.computeIfAbsent(moves) { (1..it).map(fuelPerStep).sum() }
+        val fuel = cache[moves]
         sum += fuel
         if (sum >= bestFuel) {
             return null
