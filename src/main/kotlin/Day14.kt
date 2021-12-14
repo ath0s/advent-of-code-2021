bf69e10d5ff17811d3596783b1ee2ed2b83a325a
ath0s <861482+ath0s@users.noreply.github.com>
2021-12-14 23:09:03 +0100

    Day 14 - Part Two

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -3,41 +3,61 @@ import kotlin.io.path.readLines
 
 private val rulePattern = Regex("""(\w)(\w)\s*->\s*(\w)""")
 
-fun polymerization(filename: String, steps: Int, verbose: Boolean): Int {
+fun polymerization(filename: String, steps: Int, verbose: Boolean): Long {
     val lines = filename.asPath().readLines()
     val polymerTemplate = lines.first()
 
     val pairInsertions = lines.drop(2).associate {
         val (a, b, c) = rulePattern.matchEntire(it)!!.destructured
-        (a[0] to b[0]) to "$a$c"
+        (a.single() to b.single()) to c.single()
     }
 
     if (verbose) {
         println("Template:\t$polymerTemplate")
     }
 
-    var result = polymerTemplate
-    repeat(steps) {
-        result = result.zipWithNext { a, b -> pairInsertions[a to b] }.joinToString("") + result.last()
-        if (verbose) {
-            println("After step ${it + 1}:\t$result")
+    var pairCounts = buildMap {
+        polymerTemplate.zipWithNext().forEach {
+            add(it, 1)
         }
     }
 
-    val elementCounts = result.groupingBy { it }.eachCount()
-        .toList()
-        .sortedByDescending { (_, count) -> count }
+    repeat(steps) {
+        pairCounts = pairCounts.applyInsertions(pairInsertions)
+    }
 
-    return elementCounts.first().second - elementCounts.last().second
+    val elementCounts = pairCounts.countElements(polymerTemplate.first()).values.sorted()
+
+    return elementCounts.last() -elementCounts.first()
 }
+
+private fun Map<Pair<Char, Char>, Long>.applyInsertions(pairInsertions: Map<Pair<Char, Char>, Char>) =
+    let { source ->
+        buildMap {
+            source.forEach { (pair, count) ->
+                val (a, b) = pair
+                val c = pairInsertions[pair]!!
+                add(a to c, count)
+                add(c to b, count)
+            }
+        }
+    }
+
+private fun Map<Pair<Char, Char>, Long>.countElements(firstChar: Char) =
+    (map { (pair, count) -> pair.second to count} + (firstChar to 1L))
+        .groupBy ({(char, _) -> char}, { (_, count) -> count})
+        .mapValues { (_, counts) -> counts.sum() }
+
+private fun <K> MutableMap<K, Long>.add(key: K, value: Long) =
+    compute(key) { _, previousValue -> (previousValue ?: 0) + value }
 
 class Day14 : Day {
 
     override fun partOne(filename: String, verbose: Boolean): Number =
-        polymerization(filename, 10, verbose)
+        polymerization(filename, 10, true)
 
     override fun partTwo(filename: String, verbose: Boolean): Number =
-        polymerization(filename, 0, verbose)
+        polymerization(filename, 40, verbose)
 
     companion object : Main("Day14.txt") {
 
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,6 +1,8 @@
 class Day14Test : DayTest<Day14>("Day14_test.txt") {
 
-    override val partOneExpected = 1588
+    override val partOneExpected = 1588L
+
+    override val partTwoExpected = 2188189693529L
 
 }
 
