89213aeeb5b6f367ecb8c57c60e6a1551544a459
ath0s <861482+ath0s@users.noreply.github.com>
2021-12-21 16:48:27 +0100

    Day 21 - Part Two

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -45,6 +45,46 @@ fun deterministicDice(filename: String, verbose: Boolean): Int {
     return players.minOfOrNull { it.score }!! * rolls
 }
 
+fun diracDice(filename: String, verbose: Boolean): Long {
+    val (player1, player2) = filename.asPath().readLines().map { it.toPlayerSnapshot() }
+    val cache = mutableMapOf<Pair<PlayerSnapshot, PlayerSnapshot>, Pair<Long, Long>>()
+    val (player1Wins, player2Wins) = diracDice(player1, player2, cache)
+    return maxOf(player1Wins, player2Wins)
+}
+
+private fun diracDice(playerA: PlayerSnapshot, playerB: PlayerSnapshot, cache: MutableMap<Pair<PlayerSnapshot, PlayerSnapshot>, Pair<Long, Long>>): Pair<Long, Long> {
+    if (playerA.score >= 21) {
+        return 1L to 0L
+    }
+    if (playerB.score >= 21) {
+        return 0L to 1L
+    }
+    val cacheKey = playerA to playerB
+    cache[cacheKey]?.let {
+        return it
+    }
+    var wins = 0L to 0L
+
+    (1..3).forEach { roll1 ->
+        (1..3).forEach { roll2 ->
+            (1..3).forEach { roll3 ->
+                val newPosition = (playerA.position + roll1 + roll2 + roll3) % 10
+                val newPlayerA = PlayerSnapshot(newPosition, (playerA.score + newPosition) + 1)
+
+                val newWins = diracDice(playerB, newPlayerA, cache)
+
+                wins = wins.copy(
+                    wins.first + newWins.second,
+                    wins.second + newWins.first
+                )
+            }
+        }
+    }
+
+    cache[cacheKey] = wins
+    return wins
+}
+
 private class Player(
     val name: String,
     var position: Int,
@@ -56,13 +96,24 @@ private fun String.toPlayer() =
         Player(name, position.toInt() - 1)
     }
 
+private data class PlayerSnapshot(
+    val position: Int,
+    val score: Int = 0
+)
+
+private fun String.toPlayerSnapshot() =
+    playerPattern.matchEntire(this)!!.destructured.let { (_, position) ->
+        PlayerSnapshot(position.toInt() - 1)
+    }
+
+
 class Day21 : Day {
 
     override fun partOne(filename: String, verbose: Boolean): Number =
         deterministicDice(filename, verbose)
 
     override fun partTwo(filename: String, verbose: Boolean): Number =
-        deterministicDice(filename, verbose)
+        diracDice(filename, verbose)
 
     companion object : Main("Day21.txt") {
 
--- src/test/kotlin/Day21Test.kt
+++ src/test/kotlin/Day21Test.kt
@@ -2,5 +2,6 @@ class Day21Test : DayTest<Day21>("Day21_test.txt") {
 
     override val partOneExpected = 739785
 
+    override val partTwoExpected = 444356092776315
 
 }
